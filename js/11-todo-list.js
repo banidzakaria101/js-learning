52a8a9e9b62d6a3906084cf3ba0255a6bfdbd1de
zakariabanid101 <banidzakaria2@gmail.com>
2025-09-28 00:09:05 +0100

    complete task project

--- js/11-todo-list.js
+++ js/11-todo-list.js
@@ -1,15 +1,21 @@
-const todoList = ["rrr", "eeeee"];
+const todoList = [];
+
+
 renderTodoList();
 
-function renderTodoList () {
+function renderTodoList() {
   let todoListHTML = '';
   const paragraphConatainer = document.querySelector(".js-todo-list");
-for (const task of todoList) {
-  const para = document.createElement("p");
+for (let i = 0; i < todoList.length; i++) {
+  
   //para.textContent = task;
   //paragraphConatainer.appendChild(para);
+  const task = todoList[i];
+  const { name, date } = task;
   console.log(task);
-  const html = `<p>${task}<p/>`;
+  const html = `<div>${name}</div> 
+                <div>${date}</div> 
+                <button class="delete-button" onClick="todoList.splice(${i}, 1); renderTodoList()">Delete</button>`;
   todoListHTML += html;
 }
 
@@ -19,10 +25,15 @@ document.querySelector('.js-todo-list').innerHTML = todoListHTML
 
 function addTodo() {
 
-  const inputElement = document.querySelector(".todo-input");
-  todoList.push(inputElement.value);
+  const nameElement = document.querySelector(".todo-input");
+  const dateElement = document.querySelector(".todo-date");
+  let taksObject = {
+  name: nameElement.value,
+  date: dateElement.value,
+}
+  todoList.push(taksObject);
   console.log(todoList);
-  inputElement.value = "";
+  nameElement.value = "";
 
   const pElemment = document.querySelector(".p-todo");
   renderTodoList();
